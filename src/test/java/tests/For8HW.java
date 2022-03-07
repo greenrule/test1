11eb6a0ffff2b3dc98b06e1dcf2b1e73e93197b3
greenrule <sms2004@yandex.ru>
2022-03-08 00:08:50 +0500

    add class for 8 HW

---
+++ src/test/java/tests/For8HW.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class For8HW {
+    @Test
+    void assertTrueTest() {
+        assertTrue(3 > 1);
+    }
+}
